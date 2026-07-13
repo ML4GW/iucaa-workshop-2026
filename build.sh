53cb29239f8094e8df8a67e03c4f85bf1c3a8031
deepchatterjeeligo <deepchatterjeeligo@users.noreply.github.com>
2026-07-13 17:30:05 +0000

    deploy: 205936ab2b08f5016040a76dc4de02749ac67b25

---
+++ build.sh
@@ -0,0 +1,2 @@
+# build and tag the docker image
+docker build -t iucaa-workshop-website .
---
+++ serve.sh
@@ -0,0 +1,2 @@
+# serve the website locally
+docker run --rm -v "$PWD:/srv/jekyll" -p 4000:4000 -it iucaa-workshop-website
